53243a0505b8ff13f97e4cc7bc400d7649364e3a
spawncreeper2006 <97362498+spawncreeper2006@users.noreply.github.com>
2023-01-10 10:59:17 +0000

    Update GameWorld.py

--- GameWorld.py
+++ GameWorld.py
@@ -5,6 +5,10 @@ import time
 
 
 HEIGHT = WIDTH = 800
+LEFT = WIDTH * -0.5
+RIGHT = WIDTH * 0.5
+TOP = HEIGHT * 0.5
+BOTTOM = HEIGHT * -0.5
 
 class Game:
     def __init__(self):
@@ -46,6 +50,77 @@ class Game:
 
             self.food.set_move(True)
             self.snake.grow()
+
+        condition1 = self.snake.snake_head.xcor() > RIGHT
+        condition2 = self.snake.snake_head.xcor() < LEFT
+        condition3 = self.snake.snake_head.ycor() > TOP
+        condition4 = self.snake.snake_head.ycor() < BOTTOM
+
+        if any((condition1, condition2, condition3, condition4)):
+            time.sleep(1)
+
+            self.snake.snake_die()
+
+        if self.snake.head_and_body_coll_check() == True:
+            time.sleep(1)
+            self.snake.snake_die()
+
+
+            
+
+            
+Game().RunGame()
+        self.window = Window(WIDTH, HEIGHT)
+        self.snake = Snake()
+        self.food = Food()
+
+
+    def keyboardlistener(self):
+        self.window.screen.listen()
+        self.window.screen.onkey(self.snake.go_up, 'Up')
+        self.window.screen.onkey(self.snake.go_down, 'Down')
+        self.window.screen.onkey(self.snake.go_right, 'Right')
+        self.window.screen.onkey(self.snake.go_left, 'Left')
+
+
+
+        
+    def RunGame(self):
+
+        while True:
+            self.window.screen.update()
+            self.keyboardlistener()
+            self.snake.update()
+
+
+            self.food.update()
+
+            self.world_update()
+            
+            time.sleep(0.1)
+
+            
+
+
+    def world_update(self):
+        if self.snake.snake_head.distance(self.food.item) < 15:
+
+            self.food.set_move(True)
+            self.snake.grow()
+
+        condition1 = self.snake.snake_head.xcor() > RIGHT
+        condition2 = self.snake.snake_head.xcor() < LEFT
+        condition3 = self.snake.snake_head.ycor() > TOP
+        condition4 = self.snake.snake_head.ycor() < BOTTOM
+
+        if any((condition1, condition2, condition3, condition4)):
+            time.sleep(1)
+
+            self.snake.snake_die()
+
+        if self.snake.head_and_body_coll_check() == True:
+            time.sleep(1)
+            self.snake.snake_die()
 
 
             
