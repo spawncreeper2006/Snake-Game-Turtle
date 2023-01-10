fb31794a2d01044e3d7c1ff6b43a35d0f5d68fa4
spawncreeper2006 <97362498+spawncreeper2006@users.noreply.github.com>
2023-01-10 11:00:32 +0000

    Update Window.py

--- Window.py
+++ Window.py
@@ -1,6 +1,7 @@
 import turtle
 
 
+image = 'apple.gif'
 
 class Window:
     def __init__(self, width, height):
@@ -9,5 +10,19 @@ class Window:
         self.screen.bgcolor('blue')
         self.screen.setup(width, height)
         self.screen.tracer(0)
+
+        self.screen.addshape(image)
+
+        self.hud = turtle.Turtle()
+
+    def drawHUD(self, score):
+
+        self.hud.clear()
+        self.hud.color('white')
+        self.hud.penup()
+        self.hud.hideturtle()
+        self.hud.goto(0,-400)
+
+        self.hud.write('Score: %d High Score: 0' %(score), align = 'center', font=(
 
         
