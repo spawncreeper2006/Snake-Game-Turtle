48d8a79ac8ac1661480f4b6b0182ed73779c6d1e
spawncreeper2006 <97362498+spawncreeper2006@users.noreply.github.com>
2023-01-11 09:58:29 +0000

    Update Window.py

--- Window.py
+++ Window.py
@@ -3,24 +3,36 @@ import turtle
 
 
 class Window:
-    def __init__(self, width, height):
+    def __init__(self, WIDTH, HEIGHT):
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
         self.screen = turtle.Screen()
         self.screen.title('OOP Snake')
         self.screen.bgcolor((0.5, 0, 0.5))
-        self.screen.setup(width, height)
+        self.screen.setup(WIDTH, HEIGHT)
         self.screen.tracer(0)
+
+        with open('high_score.txt') as f:
+            self.high_score = int(f.read())
+            print ('READ "high_score.txt"')
+            
 
 
         self.hud = turtle.Turtle()
 
     def drawHUD(self, score):
-
+        if score > self.high_score:
+            self.high_score = score
+            with open('high_score.txt', 'w') as f:
+                f.write(str(score))
+                print ('WRITE "high_score.txt"')
         self.hud.clear()
         self.hud.color('white')
         self.hud.penup()
         self.hud.hideturtle()
-        self.hud.goto(0,-400)
+        frac= (3, 7)
+        self.hud.goto(0,-frac[0]*self.HEIGHT//frac[1])
 
-        self.hud.write('Score: %d High Score: 0' %(score), align = 'center', font=('Courier', 22, 'normal'))
+        self.hud.write(f'Score: {score} High Score: {self.high_score}', align = 'center', font=('Courier', 22, 'normal'))
 
         
